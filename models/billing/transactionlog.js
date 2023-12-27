d32f64bb25c4091e9268db769baf6fc8fcf66924
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2023-12-27 17:17:08 +0500

    Update biling models add other fields

--- models/billing/transactionlog.js
+++ models/billing/transactionlog.js
@@ -1,112 +1,113 @@
-const { Schema, model } = require('mongoose')
-const transactionlogSchema = new Schema({
+const { Schema, model } = require("mongoose");
+const transactionlogSchema = new Schema(
+  {
     //ID расчетного счета
     payment_order_number: {
-        type: String,
-        required: true   
+      type: String,
+      required: true,
     },
 
-    typeofdistribute:[{
+    typeofdistribute: [
+      {
         type: Schema.Types.ObjectId,
-        ref: 'Typeofdistribute',
-        required: true
-    }],
-    
+        ref: "Typeofdistribute",
+        required: true,
+      },
+    ],
+
     //ID счета по дебету
     debt_account_ID: {
-        type: String       
+      type: String,
     },
 
     //ID объекта для субконто 1 счета по дебету
     debt_account_obj1_ID: {
-        type: String        
+      type: String,
     },
     //ID объекта для субконто 2 счета по дебету filieal
     debt_account_obj2_ID: {
-        type: Schema.Types.ObjectId,
-        ref: 'Breanches'
-       
+      type: Schema.Types.ObjectId,
+      ref: "Branches",
     },
     //ID объекта для субконто 3 счета по дебету Client
     debt_account_obj3_ID: {
-        type: Schema.Types.ObjectId,
-        ref: 'Agents'
-        
+      type: Schema.Types.ObjectId,
+      ref: "Agents",
     },
     //ID объекта для субконто 4 счета по дебету
     debt_account_obj4_ID: {
-        type: Schema.Types.ObjectId,
-        ref: 'Agreements'
-       
+      type: Schema.Types.ObjectId,
+      ref: "Agreements",
     },
     //ID объекта для субконто 5 счета по дебету
     debt_account_obj5_ID: {
-        type: Schema.Types.ObjectId,
-        ref: 'Policy'
-        
+      type: Schema.Types.ObjectId,
+      ref: "Policy",
     },
-    
+
     //ID счета по кредиту
     cred_account_ID: {
-        type: String
-        
+      type: String,
     },
 
     //ID объекта для субконто 1 счета по кредиту
     cred_account_obj1_ID: {
-        type: String       
+      type: String,
     },
     //ID объекта для субконто 2 счета по кредиту
     cred_account_obj2_ID: {
-        type: Schema.Types.ObjectId,
-        ref: 'Breanches'        
+      type: Schema.Types.ObjectId,
+      ref: "Branches",
     },
     //ID объекта для субконто 3 счета по кредиту
     cred_account_obj3_ID: {
-        type: Schema.Types.ObjectId,
-        ref: 'Agents'        
+      type: Schema.Types.ObjectId,
+      ref: "Agents",
     },
     //ID объекта для субконто 4 счета по кредиту
     cred_account_obj4_ID: {
-        type: Schema.Types.ObjectId,
-        ref: 'Agreements'       
+      type: Schema.Types.ObjectId,
+      ref: "Agreements",
     },
     //ID объекта для субконто 5 счета по кредиту
     cred_account_obj5_ID: {
-        type: Schema.Types.ObjectId,
-        ref: 'Policy'        
+      type: Schema.Types.ObjectId,
+      ref: "Policy",
     },
-    
+
     //сумма операции
     amount: {
-        type: String
-        
+      type: String,
     },
     //дата проводки
     transaction_date: {
-        type: Date
-       
+      type: Date,
     },
     //описание
     description: {
-        type: String
-        
+      type: String,
     },
     //дата фактического ввода операции
     input_date: {
-        type: Date
-        
-    },    
+      type: Date,
+    },
     statusofAttachment: {
-        type: String,
-        enum: ['Новый', 'В процессе', 'Готов'],
-        default: 'Новый'
+      type: String,
+      enum: ["Новый", "В процессе", "Готов"],
+      default: "Новый",
     },
     creatorId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Users',
-        required: true
-    }
-},
-    { timestamps: true })
-module.exports = model('Transactionlog', transactionlogSchema)
+      type: Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+transactionlogSchema.virtual("branch", {
+  ref: "Branches",
+  localField: "debt_account_obj2_ID",
+  foreignField: "_id",
+});
+module.exports = model("Transactionlog", transactionlogSchema);
