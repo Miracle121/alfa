9210b16b1f4fdaf4e9ee82713f21f870cd0a76aa
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2024-01-04 10:00:29 +0500

    Added details area

--- models/objects.js
+++ models/objects.js
@@ -1,21 +1,24 @@
-const {Schema,model} = require('mongoose')
+const { Schema, model } = require("mongoose");
 
-const objectsSchema = new Schema({
-    name:{
-        type:String,
-        required:true
+const objectsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    typobjectsId:{
-        type: Schema.Types.ObjectId,
-        ref: 'Typeofobjects',
-        required: true
+    typeObjectsId: {
+      type: Schema.Types.ObjectId,
+      ref: "Typeofobjects",
+      required: true,
     },
-    creatorId:{
-        type: Schema.Types.ObjectId,
-        ref: 'Users',
-        required: true
-    }  
-},
-{ timestamps:true })
+    details: Schema.Types.Mixed,
+    creatorId: {
+      type: Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = model('Objects',objectsSchema)
+module.exports = model("Objects", objectsSchema);
