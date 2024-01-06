5d1414978445da0b48ecaff9f06068a71d0feb93
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2024-01-06 14:55:56 +0500

    Update lang

--- routes/translations.js
+++ routes/translations.js
@@ -6,14 +6,12 @@ const { advancedResults } = require("../middleware/advancedResults");
 
 const router = express.Router();
 
-router.use(IsAuth);
-
 router.get("/", advancedResults(Translations), translations.getTranslations);
 router.get("/:id", translations.getTranslationsId);
 
-router.post("/", translations.createTranslations);
-router.put("/:id", translations.updateTranslations);
-router.delete("/:id", translations.deleteTranslations);
+router.post("/", IsAuth, translations.createTranslations);
+router.put("/:id", IsAuth, translations.updateTranslations);
+router.delete("/:id", IsAuth, translations.deleteTranslations);
 router.get("/lang/:id", translations.getByLanguages);
 
 module.exports = router;
